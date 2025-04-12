251815958d958affbaab6f49c06261eaca504fb7
david <david.gozlan@epitech.eu>
2025-04-12 13:16:29 +0200

    fix:backup script

--- backup.sh
+++ backup.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+TIMESTAMP=$(date +%Y_%m_%d_%H:%M:%S)
 BACKUP_DIR="./backup/$TIMESTAMP"
 
-# Home config path
-CONFIG_DIR="$HOME/.config"
-
-###########
-### SWAY
-###########
-
-# Paths
-SWAY_CONFIG="$CONFIG_DIR/sway/config"
-SWAY_BACKUP_DEST="$BACKUP_DIR/sway"
-
 # Create backup directory structure
-mkdir -p "$SWAY_BACKUP_DEST"
+mkdir -p "$BACKUP_DIR"
 
 # Backup existing Sway config
-cp "$SWAY_CONFIG" "$SWAY_BACKUP_DEST/config"
+cp "./.config/" "$BACKUP_DIR/" -r
 
 echo "📦 Backup saved in: $PWD/$SWAY_BACKUP_DEST"
 
