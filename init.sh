20770614622c4f1df1850b42f1a68d71a5438960
Votre Nom <Vous@exemple.com>
2025-04-17 15:02:53 +0200

    feat: added i3blocks

--- init.sh
+++ init.sh
@@ -16,7 +16,7 @@ sudo pacman -S \
   ttf-fira-code ttf-nerd-fonts-symbols \
   pulseaudio pavucontrol \
   swaybg wl-clipboard \
-  stow
+  stow i3blocks
 
 # Optional: swaylock-effects (prettier lock screen)
 #yay -S swaylock-effects --noconfirm
